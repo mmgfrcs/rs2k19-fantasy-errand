02d9f35d4d5889f5c7e12f0b04ed4cc079b3c8eb
Virya Dharmawan <viryadharmawan@gmail.com>
2019-03-08 14:05:06 +0700

    Verify.sh issue
    Resolved Issue in verification script

--- CI/verify.sh
+++ CI/verify.sh
@@ -14,19 +14,18 @@ grep -Fq "defaultScreenOrientation: 0" "./ProjectSettings/ProjectSettings.asset"
 echo " > Screen Orientation verified"
 echo "Verifying Project Version, expected 2018.1.1f1"
 grep -Fq "m_EditorVersion: 2018.1.1f1" "./ProjectSettings/ProjectVersion.txt"
-echo "Editor version verified"
+echo " > Editor version verified"
 echo "Verifying Assets folder structure"
 cd ./Assets/
 files=(Imports/ Materials/ Sprites/ Scripts/ Scenes/ StreamingAssets/)
-for f in $("ls -d */"); do
+for f in $(ls -d */); do
     found=0
     for g in "${files[@]}" ; do
         [ "$f" = "$g" ] && found=1
     done
-    if "$found" = 0 then
-        echo "Unexpected $f. Verification failed"
-        exit 1
+    if [ "$found" -eq 0 ]; then
+        echo "Unexpected $f. Verification failed" || exit 1
     fi
 done
-echo "Folder Structure verified"
+echo " > Folder Structure verified"
 echo "Verification Complete"

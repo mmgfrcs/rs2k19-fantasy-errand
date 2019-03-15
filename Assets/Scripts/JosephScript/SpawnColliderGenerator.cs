4381dc3f0e5763498d0318dd5bc1a221522f15d3
User Joseph D Github <joseph.ramli@binus.ac.id>
2019-03-15 15:06:44 +0700

    Added Fence and Gateway as Obstacle
    Added Fence and Gateway as Obstacle

---
+++ Assets/Scripts/JosephScript/SpawnColliderGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnColliderGenerator : MonoBehaviour {
+
+	public Transform[] spawnPos;
+	public GameObject Junction;
+	public GameObject normalRoad;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider hit){
+		if (gameObject.tag == "Start") {
+			gameObject.name="Last";
+			for(int i=0;i<10;i++){
+				GameObject newpath =(GameObject)Instantiate (Junction,spawnPos[0].transform.position,spawnPos[0].transform.rotation);
+			}
+		}
+
+		if (hit.gameObject.tag == "Player") {
+			Instantiate(Junction,spawnPos[0].transform.position,spawnPos[0].transform.rotation);
+				
+		}
+	}
+}

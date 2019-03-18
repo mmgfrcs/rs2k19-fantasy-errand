219bd6cd96e301249d4b749078b43863e5d64149
Virya Dharmawan <viryadharmawan@gmail.com>
2019-03-18 11:02:12 +0700

    Moving Stuff
    Moving Sapphiart to respetive folders
    Filling empty folders so it gets committed

--- CI/verifystruct.sh
+++ CI/verifystruct.sh
@@ -1,4 +1,28 @@
 trap "echo ' > Verification failed'" ERR
 echo "===Start Verifying Files in Folders==="
-#add verification here
+
+echo "Verifying Animations folder"
+
+find "./Unity/Research/Assets/Animations" -depth -type f  | wc -l
+
+OIFS="$IFS"
+IFS=$'\n'
+files=(meta anim controller)
+for f in $(find "./Animations" -depth -print); do
+    if [ -f "$f" ]; then
+        found=0
+        for g in "${files[@]}" ; do
+            [ "$f" = "$g" ] && found=1
+        done
+        if [ "$found" -eq 0 ]; then
+            echo "Unexpected $f. Verification failed" && exit 1
+        else
+            echo " > $f passed"
+        fi
+    fi
+done
+
+
+
+IFS="$OIFS"
 echo "Verification Complete"

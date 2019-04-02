f444eef0bcdeeb420e386a2edca73738616721e9
Joseph Ramli <joseph.ramli@binus.ac.id>
2019-04-02 13:49:33 +0700

    Adding Interface and Start Creating Static Level Manager
    - Adding interface for obstacle
    - Trying to create static level manager

---
+++ Assets/Scripts/JosephScript/MovingObstacle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FantasyErrand.Entities.Interfaces;
+public class MovingObstacle : MonoBehaviour,IObstacleMovable {
+
+	[SerializeField]
+	float moveSpeed;
+	public int minDistance;
+	public GameObject player;
+	[SerializeField]
+	private float minSpeed;
+	[SerializeField]
+	private float spawnRate;
+	public GameObject movableObject;
+	public float SpawnRate
+	{
+		get
+		{
+			return spawnRate;
+		}
+	}
+	public float MoveSpeed
+	{
+		get
+		{
+			return moveSpeed;
+		}
+	}
+
+	public void DoMove()
+	{
+		movableObject.transform.Translate (-Vector3.forward * minSpeed * Time.deltaTime, Space.World);
+		if (minSpeed < moveSpeed)
+			minSpeed++;
+	}
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (transform.position, player.transform.position) < minDistance) {
+			DoMove ();
+		}
+	}
+}
---
+++ Assets/Scripts/JosephScript/StaticObstacle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FantasyErrand.Entities.Interfaces;
+public class StaticObstacle : MonoBehaviour,IObstacle {
+	[SerializeField]
+	private float spawnRate;
+
+	public float SpawnRate 
+	{
+		get
+		{
+			return spawnRate;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

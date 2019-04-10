b1bbccd1260d7adf13006354b2566b5bad8da701
Joseph Ramli <joseph.ramli@binus.ac.id>
2019-04-10 17:10:48 +0700

    Adding Script for Obstacle and Collectible
    -Added Script in Game item Folder(CoinCollectible, PowerUpsCollectible, ObstacleGeneral)

---
+++ Assets/Scripts/GameItem/PowerUpsCollectible.cs
@@ -0,0 +1,41 @@
+﻿using FantasyErrand.Entities.Interfaces;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpsCollectible : MonoBehaviour,ICollectible{
+
+    [SerializeField]
+    private int value;
+    CollectibleType type;
+
+    public CollectibleType Type
+    {
+        get
+        {
+            return CollectibleType.Powerups;
+        }
+    }
+
+    public int Value
+    {
+        get
+        {
+            return value;
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

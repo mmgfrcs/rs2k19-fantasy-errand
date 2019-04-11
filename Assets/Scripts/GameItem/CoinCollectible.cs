898eabc9a67ac95f2ab9f175bcbe33d7e297c9d4
Joseph Ramli <joseph.ramli@binus.ac.id>
2019-04-11 15:21:59 +0700

    Implementing interface in collectible script
    Implementing interface in collectible script(coin and powerups) script

--- Assets/Scripts/GameItem/CoinCollectible.cs
+++ Assets/Scripts/GameItem/CoinCollectible.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using FantasyErrand.Entities.Interfaces;
 using FantasyErrand;
-
+using FantasyErrand.Entities;
 public class CoinCollectible : MonoBehaviour,ICollectible {
 
     [SerializeField]
     private int value;
     CollectibleType type;
-
+    bool magnetPowerUps;
+    public GameObject player;
     public CollectibleType Type
     {
         get
@@ -26,6 +27,15 @@ public class CoinCollectible : MonoBehaviour,ICollectible {
         }
     }
 
+    public int magnetRange;
+    public bool activateMagnet;
+    public float magnetSpeed;
+
+    public void CollectibleEffect()
+    {
+        throw new System.NotImplementedException();
+    }
+
     public void OnCollisionEnter(Collision collision)
     {
         GameObject obj =GameObject.Find("GameManagers");
@@ -34,11 +44,17 @@ public class CoinCollectible : MonoBehaviour,ICollectible {
 
     // Use this for initialization
     void Start () {
-		
+        player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.Rotate(0, 90*Time.deltaTime, 0);
+        if (Vector3.Distance(player.transform.position, transform.position) < magnetRange && activateMagnet)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, magnetSpeed * Time.deltaTime);
+        }
 	}
+    
+
 }

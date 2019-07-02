590ad14b84da8426c31f00067c95955744cdc970
Joseph Ramli <joseph.ramli@binus.ac.id>
2019-07-02 12:46:23 +0700

    Fixing iiii
    Fixing iiii

--- Assets/Scripts/Managers/DynamicLevelManager.cs
+++ Assets/Scripts/Managers/DynamicLevelManager.cs
@@ -686,7 +686,7 @@ namespace FantasyErrand
                                 coinAmountMod += (float)0.1;
 
                         }
-                        else if (totalPosEmotions > totalNegEmotions && totalPosEmotions>0iiiiiiiiiiiiiiiiiiii)
+                        else if (totalPosEmotions > totalNegEmotions && totalPosEmotions>0)
                         {
                             if (GetTileRate2(TileType.Obstacle) + obstacleMod < GetTileRate2(TileType.Obstacle) * 2)
                                 obstacleMod += 5;
